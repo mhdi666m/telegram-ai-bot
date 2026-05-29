89e7338451872605d7324fa58f6ffd152563266d
mhdi666m <mhdieyni7576@gmail.com>
2026-05-30 01:31:49 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-8793441411:AAGL3IEfhho4u7eWnz9kGpLfdqSWwJprj6o
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("8793441411:AAGL3IEfhho4u7eWnz9kGpLfdqSWwJprj6o")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ربات روشن شد ✅")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
